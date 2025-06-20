bc047b43481d36e0796c2469d9f98a06d4863596
vshivam05 <sv699291@gmail.com>
2025-06-20 22:06:03 +0530

    final push

--- client/src/api/api.jsx
+++ client/src/api/api.jsx
@@ -1,1 +1,3 @@
-export const API_URL = `http://localhost:5000/api`
+// export const API_URL = `http://localhost:5000/api`
+
+export const API_URL =`https://url-shortner-full-stack-z1yp.onrender.com/api`
